3e626048739bfd6f12f786f7cdac1e939468f864
Donnie Cameron <macnod@gmail.com>
2017-06-16 19:25:45 -0700

    Tweaked function documentation.

--- README.md
+++ README.md
@@ -26,5 +26,5 @@ SOFTWARE.
 ### macro  db-cmd 
 (**&REST** **ARGS**)
 
-This function executes a database command. The DS parameter is a ds data structure with the following self-describing key value pairs: :db, :username, :password, :host, :retry-count, :retry-sleep,:retry-sleep-factor, and :log-function. The :retry-sleep value is an integer that represents the number seconds to sleep after the first try. Subsequent tries multiply the last amount of sleep time by the value of :retry-sleep-factor. The log function you provide must accept a single string as a parameter. The DB-COMMAND and PARAMS parameters are a standard postmodern database command with parameters. All the parameters are required.
+(DS DB-COMMAND &REST PARAMS). This macro executes a database command. The DS parameter is a ds data structure with the following self-describing key value pairs: :db, :username, :password, :host, :retry-count, :retry-sleep,:retry-sleep-factor, and :log-function. The :retry-sleep value is an integer that represents the number seconds to sleep after the first try. Subsequent tries multiply the last amount of sleep time by the value of :retry-sleep-factor. The log function you provide must accept a single string as a parameter. The DB-COMMAND and PARAMS parameters are a standard postmodern database command with parameters. All the parameters are required.
 
--- dc-db.lisp
+++ dc-db.lisp
@@ -5,7 +5,7 @@
 (in-package :dc-db)
 
 (defmacro db-cmd (ds db-command &rest params)
-  "This function executes a database command.  The DS parameter is a ds data structure with the following self-describing key value pairs: :db, :username, :password, :host, :retry-count, :retry-sleep,:retry-sleep-factor, and :log-function.  The :retry-sleep value is an integer that represents the number seconds to sleep after the first try.  Subsequent tries multiply the last amount of sleep time by the value of :retry-sleep-factor.  The log function you provide must accept a single string as a parameter.  The DB-COMMAND and PARAMS parameters are a standard postmodern database command with parameters.  All the parameters are required."
+  "(DS DB-COMMAND &REST PARAMS). This macro executes a database command.  The DS parameter is a ds data structure with the following self-describing key value pairs: :db, :username, :password, :host, :retry-count, :retry-sleep,:retry-sleep-factor, and :log-function.  The :retry-sleep value is an integer that represents the number seconds to sleep after the first try.  Subsequent tries multiply the last amount of sleep time by the value of :retry-sleep-factor.  The log function you provide must accept a single string as a parameter.  The DB-COMMAND and PARAMS parameters are a standard postmodern database command with parameters.  All the parameters are required."
   `(loop with log = (ds-get ,ds :log-function)
       for retries-left = (1- (ds-get ,ds :retry-count)) then (1- retries-left)
       for retry-sleep = (ds-get ,ds :retry-sleep) then
